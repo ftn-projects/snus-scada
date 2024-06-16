3ffe6628c56bc8335bd0775a83cdb2213e0b1c1b
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2024-06-16 17:49:52 +0200

    [Add] add trending and driver interfaces

---
+++ SCADACore/Infrastructure/Domain/Tag/InputTagValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+using SCADACore.Infrastructure.Domain.Enumeration;
+
+namespace SCADACore.Infrastructure.Domain.Tag
+{
+    [DataContract]
+    public class InputTagValue
+    {
+        [DataMember]
+        public string TagName { get; set; }
+        [DataMember]
+        public string Value { get; set; }
+        [DataMember]
+        public DriverType DriverType { get; set; }
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ SCADACore/Infrastructure/ITrending.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using SCADACore.Infrastructure.Domain.Tag;
+
+namespace SCADACore.Infrastructure
+{
+    [ServiceContract(CallbackContract = typeof(ITrendingCallback))]
+    internal interface ITrending
+    {
+        [OperationContract(IsInitiating = true)]
+        void InitTrending();
+    }
+
+    [ServiceContract]
+    internal interface ITrendingCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void OnTrendingTagPrint(InputTagValue value);
+    }
+}
