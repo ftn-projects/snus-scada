b065320a5d714eb229a6850dc7aed5e58d7432d3
Masa Ivanov <masa.ivanov04.10@gmail.com>
2024-06-16 18:48:25 +0200

    Add trending implementation

--- Trending/Program.cs
+++ Trending/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using Trending.ServiceReference;
 
 namespace Trending
 {
     internal class Program
     {
+        public class TrendingCallback : ITrendingCallback
+        {
+            public void OnTrendingTagPrint(InputTagValue value)
+            {
+                Console.WriteLine($"Tag Name: {value.TagName}");
+                Console.WriteLine($"Value: {value.Value}");
+                Console.WriteLine($"Driver type: {value.DriverType}");
+                Console.WriteLine($"Time: {value.Timestamp}");
+                Console.WriteLine("--------------------------------------");
+            }
+        }
+
         static void Main(string[] args)
         {
+            var ic = new InstanceContext(new TrendingCallback());
+            var client = new TrendingClient(ic);
+
+            Console.WriteLine("\n---------------Trending---------------");
+
+            client.InitTrending();
+            Console.ReadKey();
         }
     }
 }
