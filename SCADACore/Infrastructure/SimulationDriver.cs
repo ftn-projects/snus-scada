99613073ecddd989058fd7e44f26bf36090b831c
Arezinko <arezinamilann@gmail.com>
2024-06-18 16:42:42 +0200

    Add Simulation Driver

---
+++ SCADACore/Infrastructure/SimulationDriver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCADACore.Infrastructure
+{
+    public class SimulationDriver :  IDriver
+    {
+
+        public double ReadValue(int address)
+        {
+            switch (address)
+            {
+                case 1
+                    return Sine();
+                case 2:
+                    return Cosine();
+                case 3:
+                    return Ramp();
+                default:
+                    return -1000;
+            }
+        }
+
+        private static double Sine()
+        {
+            return 100 * Math.Sin((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        private static double Cosine()
+        {
+            return 100 * Math.Cos((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        private static double Ramp()
+        {
+            return 100 * DateTime.Now.Second / 60;
+        }
+    }
+}
